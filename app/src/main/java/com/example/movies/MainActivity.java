937f3a35dc5732a463db3b5b1d3e3ddc48b2125d
And <dron1515@gmail.com>
2023-01-22 15:34:24 +0300

    POJO classes

--- app/src/main/java/com/example/movies/MainActivity.java
+++ app/src/main/java/com/example/movies/MainActivity.java
@@ -11,7 +11,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // P7VS855-8QM4V7B-JQW4RN1-XK8Q243 token kinopoisk
+        // P7VS855-8QM4V7B-JQW4RN1-XK8Q243 token kinopoisk personal my
 
         // https://api.kinopoisk.dev/movie?token=P7VS855-8QM4V7B-JQW4RN1-XK8Q243&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&page=2&limit=40
     }
