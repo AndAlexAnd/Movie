cef59ae137603e6100482c8b1133c857092b55da
And <dron1515@gmail.com>
2023-03-03 16:00:49 +0300

    update api and token kinopoisk

--- app/src/main/java/com/example/movies/ApiService.java
+++ app/src/main/java/com/example/movies/ApiService.java
@@ -8,13 +8,13 @@ import retrofit2.http.Query;
 
 public interface ApiService {
 
-    @GET("movie?token=P7VS855-8QM4V7B-JQW4RN1-XK8Q243&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=30")
+    @GET("movie?token=J2Z4RW3-MNQM7SN-JP98CN4-KAW796P&field=rating.kp&search=7-10&sortField=votes.kp&sortType=-1&limit=30")
    // @GET("movie?token=P7VS855-8QM4V7B-JQW4RN1-XK8Q243&field=rating.kp&search=4-8&sortField=votes.kp&sortType=1&limit=40")
     Single<MovieResponseFromDocs> loadMovies(@Query("page") int page); // Retrofit сам поставит знак амперсанда & и = к слову page
-    @GET("movie?token=P7VS855-8QM4V7B-JQW4RN1-XK8Q243&field=id")
+    @GET("movie?token=J2Z4RW3-MNQM7SN-JP98CN4-KAW796P&field=id")
     Single<TrailerResponse> loadTrailers(@Query("search") int id); // Retrofit сам поставит знак амперсанда & и = к слову id
 
-    @GET("review?token=P7VS855-8QM4V7B-JQW4RN1-XK8Q243&field=movieId")
+    @GET("review?token=J2Z4RW3-MNQM7SN-JP98CN4-KAW796P&field=movieId")
     Single<ReviewResponse> loadReviews (@Query("search") int id); // Retrofit сам поставит знак амперсанда & и = к слову id
 }
 
